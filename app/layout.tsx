c36e9804e974d8ca3976715c863b2de50d1e5b36
adrocodes <adrocodes@users.noreply.github.com>
2023-05-07 13:09:14 +1000

    homepage and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="p-6 flex items-center justify-between">
+          <h1 className="text-xl font-bold">
+            <Link href="/">{metadata.title}</Link>
+          </h1>
+          <nav>
+            <ul className="flex text-sm">
+              <li>
+                <Link href="/hygraph" className="hover:no-underline underline px-1 transition-colors">Hygraph</Link>
+              </li>
+              <li>
+                <Link href="/datocms" className="hover:no-underline underline px-1 transition-colors">DatoCMS</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,48 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+    <div className="prose-lg max-w-3xl mx-auto px-6 py-10 lg:py-24">
+      <h2>Welcome to the Pigeon demo</h2>
+      <p>
+        This is a demo of how you can use Pigeon to create the same website using multiple CMS platforms.
+      </p>
+      <div className="flex gap-3">
+        <a href="https://www.npmjs.com/package/@adrocodes/pigeon" target="_blank" rel="noopener noreferrer" className="text-sm px-4 py-3 rounded bg-red-600 font-bold text-white">
+          npm
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+        <a href="https://github.com/adrocodes/pigeon" target="_blank" rel="noopener noreferrer" className="text-sm px-4 py-3 rounded bg-black font-bold text-white">
+          Github
         </a>
       </div>
-    </main>
+      <hr />
+      <h3>Get started</h3>
+      <pre className="bg-slate-100">
+        <code>npm i @adrocodes/pigeon zod</code>
+      </pre>
+      <p>Pigeon uses <a href="https://www.npmjs.com/package/zod" target="_blank" rel="noopener noreferrer" className="underline">zod</a> to validate incoming data from your CMS to ensure you get the data you&apos;re expecting. You can then run a `transform` on this data to ensure you&apos;re components get the props they expect.</p>
+      <p>Allowing for confidence in your data and allows you to quickly see if the content model changes.</p>
+      <p>Read more in the <a href="https://github.com/adrocodes/pigeon#readme" target="_blank" rel="noopener noreferrer" className="underline">README</a></p>
+      <h3>About the demo</h3>
+      <p>This demo shows how this works in practice by connecting to several Content Management Systems to pull in data, validate, transform and render to the same components.</p>
+      <p>In this way, your data and UI is separate allowing you to reuse UI in several projects without them being tied to how a CMS works or outputs data.</p>
+      <p>For this demo, we&apos;re using:</p>
+      <div className="flex flex-wrap gap-3">
+        <Link href="/hygraph" className="text-sm px-4 py-3 rounded bg-purple-600 font-bold text-white">Hygraph</Link>
+        <Link href="/datocms" className="text-sm px-4 py-3 rounded bg-orange-600 font-bold text-white">DatoCMS</Link>
+      </div>
+      <p>With planned demos for:</p>
+      <ul className="list-disc text-base">
+        <li>Contentful</li>
+        <li>Sanity</li>
+        <li>Prismic</li>
+      </ul>
+      <h4>Demo Links</h4>
+      <ul className="list-disc text-base">
+        <li><a href="https://github.com/adrocodes/pigeon-demo/" target="_blank" rel="noopener noreferrer" className="underline">Demo Source</a></li>
+        <li><a href="https://github.com/adrocodes/pigeon-demo/tree/main/app/datocms" target="_blank" rel="noopener noreferrer" className="underline">DatoCMS Source</a></li>
+        <li><a href="https://github.com/adrocodes/pigeon-demo/tree/main/app/hygraph" target="_blank" rel="noopener noreferrer" className="underline">Hygraph Source</a></li>
+      </ul>
+    </div>
   )
 }
